0caf3b38b7f4f5dcc48c846f2f5a777f5fc121f1
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-15 16:27:29 +0800

    The header part template

---
+++ resources/views/templates/heading.blade.php
@@ -0,0 +1,16 @@
+<section id="heading">
+    <div class="container">
+        <div class="col-md-8">
+            <img src="{{ asset('/assets/logo-heading.svg') }}" alt="logo heading" class="logo img-responsive" />
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-4">
+            <img src="{{ asset('/assets/philippines.svg') }}" alt="" class="philippines" alt="philippines heading" />
+        </div>
+    </div>
+</section>
