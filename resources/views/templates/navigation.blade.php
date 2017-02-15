ef6e897566c73437863f32a3d387dfc940224245
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-15 16:27:22 +0800

    Renamed template from heading to navigation

--- resources/views/templates/navigation.blade.php
+++ resources/views/templates/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav id="header" class="navbar navbar-laravel navbar-default">
+<nav id="navigation" class="navbar navbar-laravel navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#header .header-collapse" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation .navigation-collapse" aria-expanded="false">
                 <span class="sr-only">Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -12,28 +12,28 @@
             </a>
         </div>
 
-        <div class="collapse navbar-collapse header-collapse">
+        <div class="collapse navbar-collapse navigation-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">ABOUT</a></li>
                 <li><a href="#">MEETUPS</a></li>
                 <li><a href="#">PARTNERS &amp; AFFILIATES</a></li>
                 <li><a href="#">CAREER OPPORTUNITIES</a></li>
                 <li class="social-icon">
-                    <a href="#" class="fa-stack fa">
+                    <a href="#">
                         <span class="fa-stack fa-lg">
                             <i class="fa fa-circle fa-stack-2x"></i>
                             <i class="fa fa-facebook-f fa-stack-1x"></i>
                         </span>
-                        <span class="sr-only">FACEBOOK</span>
+                        <span class="hidden-md hidden-lg">FACEBOOK</span>
                     </a>
                 </li>
                 <li class="social-icon">
-                    <a href="#" class="fa-stack fa">
+                    <a href="#">
                         <span class="fa-stack fa-lg">
                             <i class="fa fa-circle fa-stack-2x"></i>
                             <i class="fa fa-twitter fa-stack-1x"></i>
                         </span>
-                        <span class="sr-only">TWITTER</span>
+                        <span class="hidden-md hidden-lg">TWITTER</span>
                     </a>
                 </li>
             </ul>
