84892d450d9546fa090a1f1589a67bea8639fd43
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-15 23:53:02 +0800

    Rename class

--- resources/views/components/meetups/previous.blade.php
+++ resources/views/components/meetups/previous.blade.php
@@ -1,4 +1,4 @@
-<div class="upcoming child-component">
+<div class="upcoming component-content">
     <header class="page-header text-primary text-center">
         <h3>PREVIOUS <strong>MEETUP</strong></h3>
         <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit</small>
