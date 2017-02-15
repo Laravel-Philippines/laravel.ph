b5d2da21ceaed20a04992a9d6a1ea398141bd347
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-15 16:29:14 +0800

    Added viewport and more templates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,6 +3,9 @@
 <head @yield('head-attr', '')>
     @yield('head-pre')
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>@yield('title', config('app.name'))</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <script src="https://use.fontawesome.com/498c0e1bb0.js"></script>
@@ -12,9 +15,13 @@
 
     @yield('body-pre')
 
-    @include('templates.header')
+    @include('templates.navigation')
+
+    @include('templates.heading')
 
     @yield('body')
+
+    @include('templates.footer')
 
     @include('templates.scripts')
 
