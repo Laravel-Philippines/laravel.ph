8f1946cfbbd3084e198b0d7ed990b8261c6ac447
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-15 23:51:37 +0800

    HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,22 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    protected $affiliates = [
+        'nuworks',
+        'popslide',
+        'yoyo',
+        'zendesk'
+    ];
+
+    protected $connectedLinks = [
+        'fa-facebook-f' => 'https://www.facebook.com/groups/laravelph',
+        'fa-twitter' => 'https://twitter.com/laravelph',
+        'fa-github' => 'https://github.com/Laravel-Philippines',
+        'fa-meetup' => 'https://www.meetup.com/Laravel-Philippines'
+    ];
+
     public function index()
     {
-        return view('index');
+        return view('index')->with(['affiliates' => $this->affiliates, 'connected_links' => $this->connectedLinks]);
     }
 }
