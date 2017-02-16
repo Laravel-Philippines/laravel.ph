bc025e8cf039489adc8d3376515d9c6bac609393
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-16 15:31:04 +0800

    Update js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('upcoming', require('./components/Upcoming.vue'));
 
 const app = new Vue({
     el: '#app',
