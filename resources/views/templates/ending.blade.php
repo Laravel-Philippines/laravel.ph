01aa3fbc5540768ec7c960be8dfc5ef007a29210
Jay Are Galinada <jayaregalinada@gmail.com>
2017-02-25 01:38:37 +0800

    Changes due to HomeController

--- resources/views/templates/ending.blade.php
+++ resources/views/templates/ending.blade.php
@@ -5,7 +5,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
             <div class="social-icons">
                 @foreach($connected_links as $faclass => $link)
-                <a href="{{ $link }}">
+                <a href="{{ $link['url'] }}">
                     <span class="fa-stack fa-2x">
                         <i class="fa fa-circle fa-stack-2x"></i>
                         <i class="fa fa-stack-1x {{ $faclass }}"></i>
