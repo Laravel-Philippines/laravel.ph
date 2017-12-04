72a6477d6443e620609b8f8785e849619566f354
Joe Palala <jose.palala@live.com>
2017-12-05 07:56:27 +0800

    updated lorem ipsum text

--- resources/views/templates/heading.blade.php
+++ resources/views/templates/heading.blade.php
@@ -2,12 +2,8 @@
     <div class="container">
         <div class="col-md-8">
             <img src="{{ asset('/assets/logo-heading.svg') }}" alt="logo heading" class="logo img-responsive" />
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="text-justify">We are a group of expert Laravel developers who meetup once every month in Makati to discuss Laravel concepts. We want to become the next best community for learning PHP and Laravel development. 
+Lorem ipsum dolor sit amet, consetrpisdo eiusmod quis nostrud exercitation ulamko goa polars text buy bitcoin at <a href="https://www.bitbit.cash/">bitbit</a> or <a href="https://coins.ph">coins.ph</a>. no worries put a litus mone watch it grow. If you're a great writer please email <a href="mailto:jose.palala@gmail.com">joe</a> or contact him on the facebook group to replace this text. </p>
         </div>
         <div class="col-md-4">
             <img src="{{ asset('/assets/philippines.svg') }}" alt="" class="philippines" alt="philippines heading" />
