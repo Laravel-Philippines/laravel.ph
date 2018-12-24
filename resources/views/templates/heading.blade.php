b3f27bbb8b966892934d9afdb9ec7671c7d8ce4b
root <jose.palala@live.com>
2018-12-24 05:24:47 +0000

    composer.lock update and fixing some frontend stuffs

--- resources/views/templates/heading.blade.php
+++ resources/views/templates/heading.blade.php
@@ -3,8 +3,7 @@
         <div class="col-md-8">
             <img src="{{ asset('/assets/logo-heading.svg') }}" alt="logo heading" class="logo img-responsive" />
             <p class="text-justify">We are a group of expert Laravel developers who meetup once every month in Makati to discuss Laravel concepts. We want to become the next best community for learning PHP and Laravel development. 
-Lorem ipsum dolor sit amet, consetrpisdo eiusmod quis nostrud exercitation ulamko goa polars text buy bitcoin at <a href="https://www.bitbit.cash/">bitbit</a> or <a href="https://coins.ph">coins.ph</a>. no worries put a litus mone watch it grow. If you're a great writer please email <a href="mailto:jose.palala@gmail.com">joe</a> or contact him on the facebook group to replace this text. </p>
-        </div>
+  </div>
         <div class="col-md-4">
             <img src="{{ asset('/assets/philippines.svg') }}" alt="" class="philippines" alt="philippines heading" />
         </div>
